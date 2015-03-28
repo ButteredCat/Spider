f462a69487d5bd9f4c1600061bd33e5fa815dbad
ButteredCat <yunchen.xiao@gmail.com>
2015-03-28 17:44:52 +0800

    refactoring

--- multithreads.py
+++ multithreads.py
@@ -8,9 +8,7 @@ from time import ctime
 class MT(threading.Thread):
     def __init__(self, func, args, name=''):
         threading.Thread.__init__(self)
-        self.name = name
-        self.func = func
-        self.args = args
+        self.name, self.func, self.args = name, func, args
 
     def get_result(self):
         return self.res
